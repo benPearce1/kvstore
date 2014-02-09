3e9ba5bb950e731dad52653722be4fdd403fcf9a
Ben Pearce <benpearce@yahoo.com>
2014-02-09 18:50:20 +1000

    test

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.port || 80
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(req);
+  res.end("hello");
 }).listen(port);
 console.log('Server running at http://127.0.0.1:1337/');
