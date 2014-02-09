7c5df1c4fc06c20659aa2c0b531e5a4972f4efe2
Ben Pearce <benpearce@yahoo.com>
2014-02-09 22:50:54 +1000

    update

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// var http = require('http');
+// var port = process.env.port || 80
+// http.createServer(function (req, res) {
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.end("hello from azure");
+// }).listen(port);
+// console.log('Server running at http://127.0.0.1:1337/');
+
 var http = require('http');
-var port = process.env.port || 80
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("hello from azure");
-}).listen(port);
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
